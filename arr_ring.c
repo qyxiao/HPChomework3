4049013099e0e15835601d15b1617fdb3252ebd7
qyxiao <qx344@nyu.edu>
2017-03-30 23:37:36 +0800

    problem fixed

--- arr_ring.c
+++ arr_ring.c
@@ -33,28 +33,41 @@ int main( int argc, char *argv[])
   int* message_out;
   int* message_in;
   tag = 99;
-  int arrSize = 2000;  /* 524288 */
+  int arrSize = 20;  /* 524288 */
+  message_out = malloc(sizeof(int)*arrSize);
+  message_in = malloc(sizeof(int)*arrSize); 
   
   while(count_round<round_Num){
     if(rank  == 0)
   {
     destination = rank + 1;
     origin = size - 1;
-    
-      count_round++;
-      if(count_round==1){
-        message_out = malloc(sizeof(int)*arrSize);
+     
+      if(count_round==0){
         int index=0; 
         for(index=0;index<arrSize;index++){
           message_out[index]=index;
-          MPI_Send(message_out, arrSize, MPI_INT, destination, tag, MPI_COMM_WORLD);
         }
       }else{
-        message_in = malloc(sizeof(int)*arrSize);
+        message_out = message_in;
+      }
+        MPI_Send(message_out, arrSize, MPI_INT, destination, tag, MPI_COMM_WORLD);
         MPI_Recv(message_in,  arrSize, MPI_INT, origin,      tag, MPI_COMM_WORLD, &status);
         printf("At round %d the main thread receive message: %d\n", count_round, message_in[0]);
-        message_in[0]=2;
+        /*message_in[0]=2;*/
         MPI_Send(message_in, arrSize, MPI_INT, destination, tag, MPI_COMM_WORLD);
+      
+      count_round++;
+      if(count_round>=round_Num){
+        FILE* fd = NULL;
+        char filename[256];
+        snprintf(filename, 256, "output%02d.txt", count_round);
+        fd = fopen(filename,"w+");
+        int n;
+        for(n = 0; n < arrSize; ++n)
+          fprintf(fd, "  %d\n", message_in[n]);
+
+        fclose(fd);
       }   
   }
   else if(rank==size-1)
@@ -62,20 +75,18 @@ int main( int argc, char *argv[])
     destination = 0;
     origin = rank -1;
     count_round++;
-    message_in = malloc(sizeof(int)*arrSize);
-    MPI_Recv(message_in,  arrSize, MPI_INT, origin,      tag, MPI_COMM_WORLD, &status);
+    MPI_Recv(message_in,  arrSize, MPI_INT, origin, tag, MPI_COMM_WORLD, &status);
     MPI_Send(message_in, arrSize, MPI_INT, destination, tag, MPI_COMM_WORLD);
-    printf("thread %d the output message is: %d\n", rank,message_in[rank]);
+    printf("thread %d the output message is: %d\n", rank,message_in[1]);
   }
   else
   {
     destination = rank +1;
     origin = rank -1;
     count_round++;
-    message_in = malloc(sizeof(int)*arrSize);
     MPI_Recv(message_in, arrSize, MPI_INT, origin,      tag, MPI_COMM_WORLD, &status);
     MPI_Send(message_in, arrSize, MPI_INT, destination, tag, MPI_COMM_WORLD);
-    printf("thread %d the output message is: %d\n", rank, message_in[rank]);
+    printf("thread %d the output message is: %d\n", rank, message_in[2]);
   }
   
   }
